f0cd46ea94fdf93df3c340dd4703aedf4ee68118
Cuong <cuong10d4hungvuongk13@gmail.com>
2025-07-22 15:37:14 +0700

    Update env & gitignore

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
+
+//khai bao dotenv
+require('dotenv').config()
+
 //khaibao route
 const route = require('./routes/client/index.route')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT
 
 //khai bao pug
 app.set('views', './views')
